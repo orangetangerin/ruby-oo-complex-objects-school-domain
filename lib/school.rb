defc076144e1f49b82b9a36de0a84bc0c1db6f99
Jonathan Yu <jonyu02@gmail.com>
2021-06-09 16:41:08 -0400

    commit tree b5f9bf9643b3836b991237f540846ed88376c8a1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+require 'pry'
+
+class School
+    def initialize(name, roster = {})
+        @name = name
+        @roster = roster
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if !@roster[grade]  
+            @roster[grade] = []
+        end
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, students|
+            students.sort!
+        end
+    end
+end
